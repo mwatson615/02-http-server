bfaece9dc199f9283bd9c3ac5bf13e902a402bbc
Michael Watson <m.watson.tn@gmail.com>
2017-03-23 10:03:59 -0500

    readstream setup and res.end shows html h1

--- server01.js
+++ server01.js
@@ -1,11 +1,29 @@
 'use strict';
+const {createReadStream} = require('fs')
+const readStream = createReadStream('index.html');
+
 
 const { readFile } = require('fs');
 const { createServer } = require('http');
 const server = createServer();
 
-server.on('request', (req, res) => {
-	console.log('Incoming for: ', req.url)
-	res.end('OK')
+server.on('request', ({url, method, headers}, res) => {
+	// console.log('Incoming for: ', req.url)
+	// res.end('OK')
+
+
+const path = url.slice(-1) === '/'
+? url.slice(1).concat('index.html')
+: url.slic(1)
+console.log('Request received for ' + path);
+
+readFile('index.html', (err, buff) => {
+	if (err) {
+		res.statusCode = 404;
+		console.log('response', res);
+		return res.end('no luck')
+	}
+	res.end(`<h1>Hello Worm</h1>`)
+	});
 });
 server.listen(8080)
