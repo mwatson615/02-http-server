189417d422b49450db655a6dc4947692108759c6
Michael Watson <m.watson.tn@gmail.com>
2017-03-23 11:40:50 -0500

    exercise complete, shows node logo

--- server01.js
+++ server01.js
@@ -1,21 +1,25 @@
 'use strict';
-const {createReadStream} = require('fs')
+const {createReadStream, readFile} = require('fs')
 const readStream = createReadStream('index.html');
 
-
-const { readFile } = require('fs');
 const { createServer } = require('http');
 const server = createServer();
 
-server.on('request', ({url, method, headers}, res) => {
-	// console.log('Incoming for: ', req.url)
-	// res.end('OK')
+server.on('request', (req, res) => {
+// readStream
+// .on('error', (err) => {
+// 	res.end(error)
+// })
+// .on('open', () => {
+// 	readStream.pipe(res)
+// })
+//readStream emits an event called close, which calls res.end
 
+// const path = url.slice(-1) === '/'
+// ? url.slice(1).concat('index.html')
+// : url.slic(1)
+// console.log('Request received for ' + path);
 
-const path = url.slice(-1) === '/'
-? url.slice(1).concat('index.html')
-: url.slic(1)
-console.log('Request received for ' + path);
 
 readFile('index.html', (err, buff) => {
 	if (err) {
@@ -23,7 +27,7 @@ readFile('index.html', (err, buff) => {
 		console.log('response', res);
 		return res.end('no luck')
 	}
-	res.end(`<h1>Hello Worm</h1>`)
+	res.end(buff, `<h1>Hello Worm</h1>`)
 	});
 });
 server.listen(8080)
